d52078788d16d3911583cbf3cffdf1370385a0fd
tz70s <su3g4284zo6y7@gmail.com>
2017-03-04 22:11:49 +0800

    add k8s create pod

--- Kube-Plugin/kube-injector-plugin.go
+++ Kube-Plugin/kube-injector-plugin.go
@@ -1,30 +1,72 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"strconv"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/tools/clientcmd"
 	"time"
 )
 
+func createNew() *v1.Pod {
+
+	var newPod v1.Pod
+	newPod.TypeMeta.Kind = "Pod"
+	newPod.TypeMeta.APIVersion = "v1"
+	newPod.ObjectMeta.Name = "node-server"
+	newPod.ObjectMeta.Namespace = "default"
+
+	var podSpec v1.PodSpec
+
+	var conSpec v1.Container
+
+	conSpec.Name = "test"
+	conSpec.Image = "tz70s/node-server"
+
+	var conPort v1.ContainerPort
+	conPort.ContainerPort = 8080
+	conSpec.Ports = []v1.ContainerPort{conPort}
+	podSpec.Containers = []v1.Container{conSpec}
+
+	newPod.Spec = podSpec
+
+	return &newPod
+}
+
 func main() {
-	args := os.Args
-	if len(args) < 2 {
-		os.Exit(1)
+
+	kubeconfig := flag.String("kubeconfig", "/home/tzuchiao/.kube/config", "absolute path to kube config file")
+	flag.Parse()
+
+	// uses the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
 	}
-	apiServer := os.Args[1] + "/api/v1/pods"
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset.CoreV1().Pods("default").Create(createNew())
 
 	for {
-		resp, err := http.Get("http://" + apiServer)
+		pods, err := clientset.CoreV1().Pods("default").List(v1.ListOptions{})
 		if err != nil {
-			fmt.Println("Can't find the apiServer")
-		} else {
-			body, _ := ioutil.ReadAll(resp.Body)
-			fmt.Println(strconv.Itoa(resp.StatusCode) + " : " + string(body))
+			panic(err.Error())
 		}
-	}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-	time.Sleep(1000 * time.Millisecond)
+		for i := 0; i < len(pods.Items); i++ {
+			fmt.Println("Pod Namespace : " + pods.Items[i].Namespace)
+			fmt.Println("Pod name : " + pods.Items[i].Name)
+			fmt.Println("Pod Labels : ")
+			labels := pods.Items[i].Labels
+			fmt.Println(labels)
+			fmt.Println(pods.Items[i].Spec)
+		}
+		time.Sleep(5 * time.Second)
+	}
 }

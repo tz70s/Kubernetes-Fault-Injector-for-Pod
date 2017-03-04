c912d8a057667a95716ba5b6f668f551476529c9
tz70s <su3g4284zo6y7@gmail.com>
2017-03-05 02:40:40 +0800

    finish simple inject

--- Kube-Plugin/kube-injector-plugin.go
+++ Kube-Plugin/kube-injector-plugin.go
@@ -9,6 +9,8 @@ import (
 	"time"
 )
 
+// Sample create
+
 func createNew() *v1.Pod {
 
 	var newPod v1.Pod
@@ -16,7 +18,9 @@ func createNew() *v1.Pod {
 	newPod.TypeMeta.APIVersion = "v1"
 	newPod.ObjectMeta.Name = "node-server"
 	newPod.ObjectMeta.Namespace = "default"
-
+	newPod.ObjectMeta.Labels = map[string]string{
+		"injector": "false",
+	}
 	var podSpec v1.PodSpec
 
 	var conSpec v1.Container
@@ -34,8 +38,32 @@ func createNew() *v1.Pod {
 	return &newPod
 }
 
+// Update Pod with fault-injector container
+func addinjector(originalPod *v1.Pod) *v1.Pod {
+
+	var conSpec v1.Container
+	conSpec.Name = "injector"
+	conSpec.Image = "tz70s/fault-injector"
+	var conPort v1.ContainerPort
+	conPort.ContainerPort = 8282
+	conSpec.Ports = []v1.ContainerPort{conPort}
+
+	var newPod v1.Pod
+	newPod.TypeMeta.Kind = "Pod"
+	newPod.TypeMeta.APIVersion = "v1"
+	newPod.ObjectMeta.Name = originalPod.ObjectMeta.Name + "-inject"
+	newPod.ObjectMeta.Namespace = "default"
+	newPod.ObjectMeta.Labels = map[string]string{
+		"injector": "true",
+	}
+
+	newPod.Spec.Containers = append(originalPod.Spec.Containers, conSpec)
+	return &newPod
+}
+
 func main() {
 
+	// set kube-apiserver config file with address, ca, auth-token, etc.
 	kubeconfig := flag.String("kubeconfig", "/home/tzuchiao/.kube/config", "absolute path to kube config file")
 	flag.Parse()
 
@@ -50,23 +78,28 @@ func main() {
 		panic(err.Error())
 	}
 
+	// create sample pod
+
 	clientset.CoreV1().Pods("default").Create(createNew())
 
+	// polling api server until if there have pods with labels - injector = false
 	for {
 		pods, err := clientset.CoreV1().Pods("default").List(v1.ListOptions{})
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
 		for i := 0; i < len(pods.Items); i++ {
-			fmt.Println("Pod Namespace : " + pods.Items[i].Namespace)
-			fmt.Println("Pod name : " + pods.Items[i].Name)
-			fmt.Println("Pod Labels : ")
-			labels := pods.Items[i].Labels
-			fmt.Println(labels)
-			fmt.Println(pods.Items[i].Spec)
+			if pods.Items[i].Labels["injector"] == "false" {
+				tmpPod, _ := clientset.CoreV1().Pods("default").UpdateStatus(&pods.Items[i])
+				if tmpPod.Status.Phase == v1.PodRunning {
+					clientset.CoreV1().Pods("default").Delete(tmpPod.Name, &v1.DeleteOptions{})
+					// wait a little bit for status trans to terminate
+					fmt.Println("Add Injector!")
+					// async handle create
+					go clientset.CoreV1().Pods("default").Create(addinjector(tmpPod))
+				}
+			}
 		}
-		time.Sleep(5 * time.Second)
+		time.Sleep(2 * time.Second)
 	}
 }
